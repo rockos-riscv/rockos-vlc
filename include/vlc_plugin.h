d8f51a587f64c97a1316118ee1a8f16bd5952772
Sebastian Ramacher <sramacher@debian.org>
2024-02-28 23:55:22 +0100

    Bump module ABI for time_t transition

--- include/vlc_plugin.h
+++ include/vlc_plugin.h
@@ -188,8 +188,8 @@ enum vlc_module_properties
 /**
  * Current plugin ABI version
  */
-# define MODULE_SYMBOL 3_0_0f
-# define MODULE_SUFFIX "__3_0_0f"
+# define MODULE_SYMBOL 3_0_0ft64
+# define MODULE_SUFFIX "__3_0_0ft64"
 
 /*****************************************************************************
  * Add a few defines. You do not want to read this section. Really.
